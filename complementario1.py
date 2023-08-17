4bd0e5298bca6e5a4392d1a78951a6683a11fed6
unknown <SaraMarin@Sara.artecosta.local>
2023-08-17 08:33:33 -0300

    primer trabajo

---
+++ complementario1.py
@@ -0,0 +1,36 @@
+numero1 = 4 #Ejercico 1
+numero2= 3.4#Ejercico 1
+
+suma = numero1+numero2#Ejercico 2
+resta = numero1-numero2#Ejercico 3
+multiplicacion = numero1*numero2
+divisicion = numero1/numero2
+
+print(f'''La suma de los numeros {numero1} y {numero2 } es: {suma}
+La resta de los numeros {numero1} y {numero2 } es: {resta}
+La multiplicacion de los numeros {numero1} y {numero2 } es: {multiplicacion}
+La divisicion de los numeros {numero1} y {numero2 } es: {divisicion}
+''')
+
+nombre = 'Josue Rosales'
+precio = 34.5
+descuento = 0.45
+precio_final= precio-(precio*descuento)
+cadena = 'Hola mundo como estas?'
+longitud = len(cadena)
+precio = int(45.7)
+nombre = 'Josue'
+apellido = 'Rosales'
+nombre_completo = nombre+' '+apellido
+edad = 20
+edad +=5
+edad -=10
+altura = 1.73
+altura*=4
+divisicion/=3
+nombre_completo = 'JOSUE'
+nombre_completo = nombre_completo.lower()
+nombre_completo= 'JOSUE'
+nombre_completo = nombre_completo.capitalize()
+print(nombre_completo)
+
---
+++ primero.py
@@ -0,0 +1,28 @@
+nombre= "Josue"
+edad = 20
+estatura= 1.72
+estudia = True
+lista_compra = ['palta','bananana','piña','arroz']
+promedio_2023 = (10,6,7,5)
+var_nula = None
+numero_complejo = 1.3-4j
+paises_visitado = {
+    'Colombia':{
+        'ciudades':['Barranquilla','Santa Marta']
+        },
+    'Argentina':{
+        'ciudades':['Mendoza','Cordoba']
+        }
+}
+
+frase = "Caminante, no hay camino, se hace camino al andar."
+
+
+
+
+edad= int(input('Digite la edad para saber cuantos años le falta para llegar a 100 años: '))
+print(100+edad)
+
+numero = int(input('Digite el numero para saber si es Par o impar'))
+resultado = 'Es par' if numero%2==0 else 'Es impar'
+print(resultado)
